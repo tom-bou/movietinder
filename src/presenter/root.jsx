4051170c962610b7c13b9bdf2037a686c79a4bcd
tom-bou <tomboustedt@gmail.com>
2023-11-23 15:15:49 +0100

    format

---
+++ src/presenter/root.jsx
@@ -0,0 +1,11 @@
+import { observer } from 'mobx-react-lite';
+
+const ReactRoot = observer(({ model }) => {
+    return (
+      <div className="">
+      </div>
+    );
+    }
+  );
+  
+  export default ReactRoot;
