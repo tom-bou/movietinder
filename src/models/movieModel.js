f46110dcf76e78584ab1aebc41442eb1384b1a0a
Cornelia Kärnekull <karnekull.cornelia@gmail.com>
2023-11-24 11:12:15 +0100

    Startplan

---
+++ src/models/movieModel.js
@@ -0,0 +1,27 @@
+
+
+export default {
+    title: "",
+    picture: "",
+    trailer: "",
+    summary: "",
+    genre: "",
+    releaseyear: ""
+
+    //D
+
+
+
+    //Funktion för gilla knappen. Filmen vi är inne på ska sparas för användaren och läggas i likedmovies
+
+    //
+
+}
+
+
+
+//Knappar - Corre
+//Visa informationen för varje film
+//Visa bibilioteket med gillade filmer
+//Log in funktionen - David
+//Ladda in hemsidan funktionen (loading symbol) - Corre
---
+++ src/models/userModel.js
@@ -0,0 +1,8 @@
+
+export default {
+    username: "",
+    password: "",
+    liked_movies: [],
+
+
+}
