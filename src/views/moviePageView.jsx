7e6af588cb9b294943fe2338f04af0fc1ee7fa86
linus <linusbalt3r@gmail.com>
2023-12-05 15:59:56 +0100

    Added correct styling.

--- src/views/moviePageView.jsx
+++ src/views/moviePageView.jsx
@@ -2,7 +2,7 @@ import "../style.css"
 
 function MoviePageView(props) {
     return (
-        <div className="flex min-h-screen">
+        <div className="flex min-h-screen bg-custom-purple">
             <div className="p-1 flex-column w-1/2"> {/* Leftmost column */}
                 <div className="h-2/3 flex items-center justify-center p-6"> {/* Upper left */}
                     <div className="h-full w-full">
@@ -21,32 +21,32 @@ function MoviePageView(props) {
                 </div>
             </div>
             <div className="p-1 flex-column w-1/2"> {/* Rightmost column */}
-                <div className="h-16 flex justify-end"> {/* Upper right */}
-                    <button className="text-red-500">Your likes</button>
+                <div className="h-16 flex justify-end p-5"> {/* Upper right */}
+                    <button className="shadow-red-glow text-red-500 text-xl">Your likes</button>
                 </div>
                 <div className="h-1/2 flex m-auto p-2.5"> {/* Middle right */}
                     <div className="w-full h-full grid grid-cols-2 gap-2 text-center">
-                        <div className="bg-gray-300 p-2 m-auto">
-                            <p>Title</p>
-                            <p>65</p>
+                        <div className="p-2 m-auto">
+                            <p className="shadow-white-glow text-category-title">Title</p>
+                            <p className="text-category-body">65</p>
                         </div>
-                        <div className="bg-gray-300 p-2 m-auto">
-                            <p>Genre</p>
-                            <p>Science Fiction</p>
+                        <div className="p-2 m-auto">
+                            <p className="shadow-white-glow text-category-title">Genre</p>
+                            <p className="text-category-body">Science Fiction</p>
                         </div>
-                        <div className="bg-gray-300 p-2 m-auto">
-                            <p>Actors</p>
-                            <p>Adam Driver, Another Actor, And One More Actor</p>
+                        <div className="p-2 m-auto">
+                            <p className="shadow-white-glow text-category-title">Actors</p>
+                            <p className="text-category-body">Adam Driver, Another Actor, And One More Actor</p>
                         </div>
-                        <div className="bg-gray-300 p-2 m-auto">
-                            <p>Directors</p>
-                            <p>Scott Beck, Bryan Woods</p>
+                        <div className="p-2 m-auto">
+                            <p className="shadow-white-glow text-category-title">Directors</p>
+                            <p className="text-category-body">Scott Beck, Bryan Woods</p>
                         </div>
                     </div>
                 </div>
-                <div className="bg-gray-300 flex flex-col space-y-4"> {/* Lower right */}
-                    <p className="text-center">Storyline</p>
-                    <p>After a catastrophic crash, pilot Mills quickly discovers he's actually stranded on an unknown planet. Now, with only one chance at rescue, Mills must make his way across an unknown terrain riddled with dangerous prehistoric creatures in an epic fight to survive. From the writers of A Quiet Place comes 65, a sci-fi thriller produced by Sam Raimi, Deborah Liebling, Zainab Azizi, Scott Beck and Bryan Woods</p>
+                <div className="flex flex-col space-y-4"> {/* Lower right */}
+                    <p className="text-center text-category-title">Storyline</p>
+                    <p className="text-white text-md">After a catastrophic crash, pilot Mills quickly discovers he's actually stranded on an unknown planet. Now, with only one chance at rescue, Mills must make his way across an unknown terrain riddled with dangerous prehistoric creatures in an epic fight to survive. From the writers of A Quiet Place comes 65, a sci-fi thriller produced by Sam Raimi, Deborah Liebling, Zainab Azizi, Scott Beck and Bryan Woods</p>
                 </div>
             </div>
         </div>
