f0a237cba7fdf662b6466437a0a737b2e8b7d04e
Cornelia Kärnekull <karnekull.cornelia@gmail.com>
2023-12-07 16:44:10 +0100

    Moviepage updates

--- src/presenter/moviePagePresenter.jsx
+++ src/presenter/moviePagePresenter.jsx
@@ -1,5 +1,11 @@
 import { observer } from "mobx-react-lite";
 import MoviePageView from "../views/moviePageView.jsx";
+import loading from "../images/loading.gif"
+import logo from "../images/logo.png";
+import dislikebutton from "../images/dislikebutton.png";
+import likebutton from "../images/likebutton.png";
+import trailer from "../images/trailer.png";
+import moviegif from "../images/moviegif.gif";
 
 export default observer(function MoviePagePresenter(props) {
   function handleLikeACB(evt) {
@@ -26,10 +32,24 @@ export default observer(function MoviePagePresenter(props) {
         );
       } else {
         return (
-          <img
-            src="https://brfenergi.se/iprog/loading.gif"
-            alt="Loading..."
-          ></img>
+          <div className="flex min-h-screen" style={{ background: 'linear-gradient(to bottom, #150629 60%, #1C0A34, #5A2960)' }}>
+            <div className="p-1 flex-column w-1/2">
+              <div className="h-2/3 flex items-center justify-center p-6">
+                <div className="z-0 bg-gradient-to-br from-blue-900 via-violet-700 blur-md to-pink-800 rounded-lg absolute top-24 left-15 right-15 bottom-15 " style={{ width: '330px', height:'485px' }}></div>
+              <div className="flex justify-center absolute bottom-24 left-48 items-start space-x-6 p-5">
+                <button className="w-20 h-20 rotate-45"> <img src={dislikebutton} alt="Dislike"/></button>
+                <button className="w-20 h-20"><img src={trailer} alt="Trailer"/></button>
+                <button className=" w-24 h-20"><img src={likebutton} alt="Like"/></button>
+            </div>
+            </div>
+            <img className="w-36 absolute top-64 right-96" src="https://www.uokajk.edu.pk/data/ecounselling/assets/chat_info_icon.gif" />
+            
+
+            </div>
+
+          <img src={logo} alt="Logo icon" class="shadow-inner absolute left-2 top-7 w-40"  style={{ filter: 'drop-shadow(0 0 0.2rem #C772ED)' }} />  
+
+          </div>
         );
       }
     } else {
--- src/views/moviePageView.jsx
+++ src/views/moviePageView.jsx
@@ -1,5 +1,9 @@
 import "../style.css";
 import { useNavigate } from 'react-router-dom';
+import dislikebutton from "../images/dislikebutton.png";
+import likebutton from "../images/likebutton.png";
+import trailer from "../images/trailer.png";
+import logo from "../images/logo.png";
 
 function MoviePageView(props) {
   var image_url =
@@ -22,7 +26,7 @@ function MoviePageView(props) {
     navigate("/yourlikes");
   }
   return (
-    <div className="flex min-h-screen bg-custom-purple">
+    <div className="flex min-h-screen" style={{ background: 'linear-gradient(to bottom, #150629 60%, #1C0A34, #5A2960)' }}>
       <div className="p-1 flex-column w-1/2">
         {" "}
         {/* Leftmost column */}
@@ -31,27 +35,23 @@ function MoviePageView(props) {
           {/* Upper left */}
           <div className="h-full w-full">
             <img
-              className="m-auto max-h-full poster-edge-gradient"
+              className="absolute top-20 relative z-10 m-auto max-h-full poster-edge-gradient"
               src={image_url}
               alt="Movie poster"
             />
           </div>
-        </div>
-        <div className="h-1/3">
+            <div className="z-0 bg-gradient-to-br from-blue-900 via-violet-700 blur-md to-pink-800 rounded-lg absolute top-24 left-15 right-15 bottom-15 " style={{ width: '330px', height:'485px' }}></div>
           {" "}
           {/* Lower left */}
-          <div className="flex justify-center items-start space-x-6 p-5">
-            <button onClick={dislikeACB} className="bg-red-500 w-20 h-20 rounded-lg aspect-square">
-              Don't like
+          <div className="flex justify-center absolute bottom-24 left-48 items-start space-x-6 p-5">
+            <button onClick={dislikeACB} className="w-20 h-20 rotate-45">
+              <img src={dislikebutton} alt="Dislike"/>
             </button>
-            <button className="bg-blue-500 w-20 h-20 rounded-lg aspect-square">
-              View trailer
+            <button className="w-20 h-20">
+              <img src={trailer} alt="Trailer"/>
             </button>
-            <button
-              onClick={likeACB}
-              className="bg-green-500 w-20 h-20 rounded-lg aspect-square"
-            >
-              Like
+            <button onClick={likeACB} className=" w-24 h-20">
+              <img src={likebutton} alt="Like"/>
             </button>
           </div>
         </div>
@@ -62,8 +62,8 @@ function MoviePageView(props) {
         <div className="h-16 flex justify-end p-5">
           {" "}
           {/* Upper right */}
-          <button onClick={windowToLikes} className="shadow-red-glow text-red-500 text-xl">
-            Your likes
+          <button onClick={windowToLikes}  className="text-3xl font-thin font-sans" style={{ color: "#FF7272", textShadow: "0px 0px 4px #FF3131" }}>
+            Your likes ♡
           </button>
         </div>
         <div className="h-1/2 flex m-auto p-2.5">
@@ -71,34 +71,35 @@ function MoviePageView(props) {
           {/* Middle right */}
           <div className="w-full h-full grid grid-cols-2 gap-2 text-center">
             <div className="p-2 m-auto">
-              <p className="shadow-white-glow text-category-title">Title</p>
-              <p className="text-category-body">{props.movieData.original_title}</p>
+              <p className="shadow-white-glow text-3xl font-bold font-sans" style={{ color: "#F4FDCF", textShadow: "0px 0px 6px #FFE370" }}>TITLE</p>
+              <p className="text-category-body font-thin font-sans">{props.movieData.original_title}</p>
             </div>
             <div className="p-2 m-auto">
-              <p className="shadow-white-glow text-category-title">Genre</p>
-              <p className="text-category-body">Science Fiction</p>
+              <p className="shadow-white-glow text-3xl font-bold font-sans" style={{ color: "#F4FDCF", textShadow: "0px 0px 6px #FFE370" }}>GENRE</p>
+              <p className="text-category-body font-thin font-sans">Science Fiction</p>
             </div>
             <div className="p-2 m-auto">
-              <p className="shadow-white-glow text-category-title">Actors</p>
-              <p className="text-category-body">
+              <p className="shadow-white-glow text-3xl font-bold font-sans" style={{ color: "#F4FDCF", textShadow: "0px 0px 6px #FFE370" }}>ACTORS</p>
+              <p className="text-category-body font-thin font-sans">
                 Adam Driver, Another Actor, And One More Actor
               </p>
             </div>
             <div className="p-2 m-auto">
-              <p className="shadow-white-glow text-category-title">Directors</p>
-              <p className="text-category-body">Scott Beck, Bryan Woods</p>
+              <p className="shadow-white-glow text-3xl font-bold font-sans" style={{ color: "#F4FDCF", textShadow: "0px 0px 6px #FFE370" }}>DIRECTORS</p>
+              <p className="text-category-body font-thin font-sans">Scott Beck, Bryan Woods</p>
             </div>
           </div>
         </div>
         <div className="flex flex-col space-y-4">
           {" "}
           {/* Lower right */}
-          <p className="text-center text-category-title">Storyline</p>
-          <p className="text-white text-md">
+          <p className="text-center shadow-white-glow text-3xl font-bold font-sans"  style={{ color: "#F4FDCF", textShadow: "0px 0px 6px #FFE370" }}>STORYLINE</p>
+          <p className="text-white text-center text-md font-thin font-sans">
           {props.movieData.overview}
           </p>
         </div>
       </div>
+      <img src={logo} alt="Logo icon" class="shadow-inner absolute left-2 top-7 w-40"  style={{ filter: 'drop-shadow(0 0 0.2rem #C772ED)' }} />
     </div>
   );
 }
