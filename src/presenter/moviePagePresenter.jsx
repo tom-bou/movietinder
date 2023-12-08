d4b76302acb1bc33f74a52403f59764e87545d49
linus <linusbalt3r@gmail.com>
2023-12-08 15:00:42 +0100

    Added "loading" version of moviePageView.

--- src/presenter/moviePagePresenter.jsx
+++ src/presenter/moviePagePresenter.jsx
@@ -32,23 +32,66 @@ export default observer(function MoviePagePresenter(props) {
         );
       } else {
         return (
-          <div className="flex min-h-screen" style={{ background: 'linear-gradient(to bottom, #150629 60%, #1C0A34, #5A2960)' }}>
-            <div className="p-1 flex-column w-1/2">
-              <div className="h-2/3 flex items-center justify-center p-6">
-                <div className="z-0 bg-gradient-to-br from-blue-900 via-violet-700 blur-md to-pink-800 rounded-lg absolute top-24 left-15 right-15 bottom-15 " style={{ width: '330px', height:'485px' }}></div>
-              <div className="flex justify-center absolute bottom-24 left-48 items-start space-x-6 p-5">
-                <button className="w-20 h-20 rotate-45"> <img src={dislikebutton} alt="Dislike"/></button>
-                <button className="w-20 h-20"><img src={trailer} alt="Trailer"/></button>
-                <button className=" w-24 h-20"><img src={likebutton} alt="Like"/></button>
+          <div className="flex min-h-screen max-h-screen bg-custom-purple relative" style={{ background: 'linear-gradient(to bottom, #150629 60%, #1C0A34, #5A2960)' }}>
+            <div className="p-1 flex-column w-1/2 xl:pl-10"> {/* Leftmost column */}
+              <div className="h-2/3 flex items-center justify-center p-1 sm:pt-10 xl:h-3/4 xl:pt-10"> {/* Upper left */}
+                <div className="flex items-center justify-center h-full w-full max-h-fit max-w-fit"> {/* Div surrounding image element */}
+                  <img
+                    className="w-3/4 lg:w-1/2"
+                    src="https://www.uokajk.edu.pk/data/ecounselling/assets/chat_info_icon.gif"
+                    alt="Loading gif"
+                  />
+                </div>
+              </div>
+              <div className="h-1/3"> {/* Lower left */}  
+                <div className="flex justify-center items-start space-x-6 p-5">
+                  <button className="w-20 h-20 rotate-45">
+                    <img src={dislikebutton} alt="Dislike"/>
+                  </button>
+                  <button className="w-20 h-20">
+                    <img src={trailer} alt="Trailer"/>
+                  </button>
+                  <button className="w-24 h-24 -mt-2">
+                  <img src={likebutton} alt="Like"/>
+                  </button>
+                </div>
+              </div>
             </div>
+            <div className="flex-column w-1/2"> {/* Rightmost column */}
+              <div className="h-16 pr-3 flex justify-end"> {/* Upper right */}
+                <button className="text-3xl font-thin font-sans" style={{ color: "#FF7272", textShadow: "0px 0px 4px #FF3131" }}>
+                  Your likes ♡
+                </button>
+              </div>
+              <div className="h-1/2 flex m-auto p-2.5 mr-10 xl:-ml-12"> {/* Middle right */}
+                <div className="w-full h-full grid grid-cols-2 gap-2 text-center lg:pt-5">
+                  <div className="p-2 justify-center">
+                    <p className="shadow-white-glow text-3xl font-bold font-sans" style={{ color: "#F4FDCF", textShadow: "0px 0px 6px #FFE370" }}>TITLE</p>
+                    <p className="text-category-body font-thin font-sans"></p>
+                  </div>
+                  <div className="p-2 justify-center">
+                    <p className="shadow-white-glow text-3xl font-bold font-sans" style={{ color: "#F4FDCF", textShadow: "0px 0px 6px #FFE370" }}>GENRE</p>
+                    <p className="text-category-body font-thin font-sans"></p>
+                  </div>
+                  <div className="p-2 justify-center">
+                    <p className="shadow-white-glow text-3xl font-bold font-sans" style={{ color: "#F4FDCF", textShadow: "0px 0px 6px #FFE370" }}>ACTORS</p>
+                    <p className="text-category-body font-thin font-sans">
+
+                    </p>
+                  </div>
+                  <div className="p-2 justify-center">
+                    <p className="shadow-white-glow text-3xl font-bold font-sans" style={{ color: "#F4FDCF", textShadow: "0px 0px 6px #FFE370" }}>DIRECTORS</p>
+                    <p className="text-category-body font-thin font-sans"></p>
+                  </div>
+                </div>
+              </div>
+              <div className="flex flex-col space-y-4 pr-10 mr-6 xl:-ml-12"> {/* Lower right */}
+                <p className="text-center shadow-white-glow text-3xl font-bold font-sans"  style={{ color: "#F4FDCF", textShadow: "0px 0px 6px #FFE370" }}>STORYLINE</p>
+                <p className="text-white text-center text-lg font-thin font-sans">
+                </p>
+              </div>
             </div>
-            <img className="w-36 absolute top-64 right-96" src="https://www.uokajk.edu.pk/data/ecounselling/assets/chat_info_icon.gif" />
-            
-
-            </div>
-
-          <img src={logo} alt="Logo icon" class="shadow-inner absolute left-2 top-7 w-40"  style={{ filter: 'drop-shadow(0 0 0.2rem #C772ED)' }} />  
-
+            <img src={logo} alt="Logo icon" className="hidden xl:flex shadow-inner absolute top-7 w-40"  style={{ filter: 'drop-shadow(0 0 0.2rem #C772ED)' }} />
           </div>
         );
       }
